c22ce30f1e182f49b013d65ccfa9c60467e174d8
ozzborn1 <145194134+ozzborn1@users.noreply.github.com>
2023-10-11 11:17:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList<int> integers = new ArrayList<int>();
+            integers.Add(112);
+            integers.Add(114);
+             
+            int element = integers.RemoveAt(1);
+            int c1=integers.Count(1);
+
+            Console.WriteLine("Integers: {0}", string.Join(" ", c1));
+            
+
+           /* ArrayList<string> strings = new ArrayList<string>();
+            strings.Add("Hello");
+            strings.Add("World");
+            Console.WriteLine("Strings: {0}", string.Join(" ", strings)); */
+        }
+    }
+}
